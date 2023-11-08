71992366acf8b51a6ec0e395d33699bd70a0f87e
Edwin Giovanni <edwin@tide.mx>
2023-11-07 22:06:16 -0600

    initialization project

---
+++ src/main/java/ed/service/messaging/MessagingApplication.java
@@ -0,0 +1,14 @@
+package ed.service.messaging;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class MessagingApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MessagingApplication.class, args);
+    }
+}
